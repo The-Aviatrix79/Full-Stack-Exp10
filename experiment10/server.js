afcbe49ee38cae27a647e33178e123d1f6b4189d
Divyam Arora <divyamarora76@gmail.com>
2026-04-24 12:19:21 +0530

    first commit

---
+++ experiment10/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// REPLACE THIS STRING WITH YOUR MONGODB ATLAS CONNECTION STRING
+const MONGO_URI = "mongodb+srv://divyamarora76_db_user:database112233@test1.oho18da.mongodb.net/collegeDB?appName=Test1";
+
+mongoose.connect(MONGO_URI)
+.then(() => console.log("Database Connected"))
+.catch(err => console.log("Database Connection Error: ", err));
+
+const studentRoutes = require("./routes/studentRoutes");
+app.use("/api/students", studentRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
